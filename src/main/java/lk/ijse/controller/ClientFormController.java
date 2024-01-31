91bd98d7c8ccad29040567000f218cbcb87a5c14
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-01-31 15:39:45 +0530

    send image part start

--- src/main/java/lk/ijse/controller/ClientFormController.java
+++ src/main/java/lk/ijse/controller/ClientFormController.java
@@ -7,18 +7,26 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URL;
@@ -44,6 +52,9 @@ public class ClientFormController implements Initializable {
     @FXML
     private ScrollPane scrollPane;
 
+    @FXML
+    private AnchorPane rootNode;
+
 
     Socket socket;
 
@@ -59,8 +70,19 @@ public class ClientFormController implements Initializable {
         dataOutputStream.writeUTF(txtMessage.getText().trim());
         dataOutputStream.flush();
         txtMessage.clear();
-        Label label = new Label(message);
-        vBoxArea.getChildren().add(label);
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        hBox.setPadding(new Insets(5,5,5,10));
+
+        Text text = new Text(message);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-background-color: #81ecec;-fx-fill: #f5f6fa;-fx-font-weight: normal; -fx-background-radius: 20px");
+        textFlow.setPadding(new Insets(5,10,5,10));
+        text.setFill(Color.color(0,0,0));
+        hBox.getChildren().add(textFlow);
+
+        vBoxArea.getChildren().add(hBox);
 
     }
 
@@ -71,7 +93,41 @@ public class ClientFormController implements Initializable {
 
     @FXML
     void btnImageBrowserOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.bmp", "*.jpeg")
+        );
 
+        Stage stage = (Stage) rootNode.getScene().getWindow();
+
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            String sendImage = file.toURI().toString();
+            System.out.println("Image URL: " + sendImage);
+            sendImageToServer(sendImage);
+        }
+    }
+
+    private void sendImageToServer(String sendImage) {
+        Image image = new Image(sendImage);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(200);
+        imageView.setFitWidth(200);
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(5,5,5,10));
+        hBox.getChildren().add(imageView);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+
+        vBoxArea.getChildren().add(hBox);
+
+        try {
+            dataOutputStream.writeUTF(sendImage);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -113,18 +169,22 @@ public class ClientFormController implements Initializable {
 
     private void setMessage(String message, VBox vBoxArea) {
 
-        HBox hBoxName = new HBox();
-        hBoxName.setAlignment(Pos.CENTER_LEFT);
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5,5,5,10));
 
-        Text textName = new Text(message);
-        TextFlow textFlowName = new TextFlow(textName);
-        hBoxName.getChildren().add(textFlowName);
+        Text text = new Text(message);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-background-color: #dfe6e9; -fx-font-weight: normal; -fx-background-radius: 20px");
+        textFlow.setPadding(new Insets(5,10,5,10));
+        text.setFill(Color.color(0,0,0));
+        hBox.getChildren().add(textFlow);
 
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
 
-                vBoxArea.getChildren().add(hBoxName);
+                vBoxArea.getChildren().add(hBox);
                 //vBoxArea.getChildren().add(hBoxName);
             }
         });
